f32ecebe40c9d930cf705b4c918aa514fdd683cc
ClaasMenke <71816669+ClaasMenke@users.noreply.github.com>
2020-10-05 18:49:54 +0200

    Update Program.cs

--- TestAppHSP/TestAppHSP/Program.cs
+++ TestAppHSP/TestAppHSP/Program.cs
@@ -6,7 +6,7 @@ namespace TestAppHSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo");
+            Console.WriteLine("Hallo Welt");
             Console.ReadKey();
         }
     }
